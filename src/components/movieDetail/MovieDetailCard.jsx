b969870ce9a8c66a4a7621f2662549c965b8e5d3
farukycgl <faruk.kuy@gmail.com>
2025-09-17 16:34:35 +0300

    MovieDetailCard comp. css ve dinamiklik güncellemesi yapıldı

---
+++ src/components/movieDetail/MovieDetailCard.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { API_MOVIE_IMG } from "../../api";
+import { FaStar } from "react-icons/fa";
+
+const MovieDetailCard = ({ movieDetail }) => {
+  const {
+    title,
+    overview,
+    vote_average,
+    backdrop_path,
+    poster_path,
+    genres,
+    original_title,
+    spoken_languages,
+    release_date,
+  } = movieDetail;
+
+  const releaseDate = new Date(release_date); //Tarihi sadece year olarak tanımladık.
+
+  return (
+    <article
+      /* Arka plan resmi */
+      className="w-screen min-h-screen bg-cover bg-center relative"
+      style={{ backgroundImage: `url(${API_MOVIE_IMG}/${backdrop_path})` }}
+    >
+      <div className="bg-black/80 min-h-screen text-amber-100">
+        {/* Baslik */}
+        <header className="px-8 py-4">
+          <h1 className="text-3xl font-bold">{title}</h1>
+        </header>
+
+        <main className="flex gap-10 justify-center p-20">
+          {/* Sol taraftaki poster kismi */}
+          <div className="w-1/4 flex justify-center">
+            <img
+              className="shadow-lg rounded-2xl max-h-[500px] max-w-[350px] object-contain"
+              src={`${API_MOVIE_IMG}/${poster_path}`}
+              alt={title}
+            />
+          </div>
+
+          {/* Sag taraftaki aciklamalar */}
+          <div className="w-2/3 flex flex-col space-y-4 max-w-2xl">
+            <section className="mb-15 text-2xl text-white">
+              <p>{overview}</p>
+            </section>
+
+            <section className="flex">
+              <p className="flex items-center gap-1">
+                IMDB Raiting:
+                <span className="flex items-center gap-1 text-white">
+                  <FaStar className="text-yellow-500" />{" "}
+                  {Number(vote_average).toFixed(1)}
+                </span>
+              </p>
+            </section>
+
+            <section className="flex">
+              <p className="flex gap-1">
+                Year:
+                <span className="flex text-white">
+                  {releaseDate.getFullYear()}
+                </span>
+              </p>
+            </section>
+
+            <section className="flex gap-1">
+              <p>Genre:</p>
+              <p className="flex gap-1 text-white">
+                {genres?.map(genre => 
+                    genre.name).join(", ")
+                }
+              </p>
+            </section>
+
+            <section className="flex gap-1">
+              <p>Language: </p>
+              <p className="text-white">
+                {spoken_languages?.map(language =>
+                  language.name).join(", ")
+                }
+              </p>
+            </section>
+          </div>
+        </main>
+      </div>
+    </article>
+  );
+};
+
+export default MovieDetailCard;
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
-import FilmDetail from "../components/movieDetail/FilmDetail";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getMovieDetailById } from "../redux/slices/movieDetailSlice";
+import MovieDetailCard from "../components/movieDetail/MovieDetailCard";
 
 const MovieDetail = () => {
   const { movieDetail } = useSelector((store) => store.movieDetail);
@@ -15,8 +15,8 @@ const MovieDetail = () => {
   }, [id, dispatch]);
 
   return (
-    <div>
-      <FilmDetail movieDetail={movieDetail} />
+    <div className="">
+      <MovieDetailCard movieDetail={movieDetail} />
     </div>
   );
 };
